195ddc7ddc2f5a8f06a9c4f39f165c2e782a8b1a
Next-Token <>
2023-11-07 14:59:26 -0600

    button formation and console logging. Very ugly still.

--- fruit-cup/src/Header/Header.js
+++ fruit-cup/src/Header/Header.js
@@ -1,5 +1,12 @@
 import "./Header.css";
 
+var fruity = "https://thefruitbasket.s3.amazonaws.com/FruitLogo.png"
+
+
+const handleClick = () => {
+  console.log('hewwo wold');
+}
+
 function Header() {
   return (
     <heading>
@@ -7,10 +14,10 @@ function Header() {
             <img id="Logo" src="https://thefruitbasket.s3.amazonaws.com/FruitLogo.png" />
             <div className="Nav-Banner-ele">
               
-            <h1 id="Nav1">Home</h1>
-            <h1 id="Nav2">About</h1>
-            <h1 id="Nav3">Seth</h1>
-            <h1 id="Nav4">Projects</h1>
+            <button onClick={handleClick}><h1 id="Nav1">Home</h1></button>
+            <button><h1 id="Nav2">About</h1></button>
+            <button><h1 id="Nav3">Seth</h1></button>
+            <button><h1 id="Nav4">Projects</h1></button>
 
             </div>
         </div>
