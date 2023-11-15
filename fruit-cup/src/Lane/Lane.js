4e366127a2f21733b97dcb524d8734140a17ec91
Next-Token <SSerpas@ladb.org>
2023-11-15 15:20:18 -0600

    transforming fruit positions

--- fruit-cup/src/Lane/Lane.js
+++ fruit-cup/src/Lane/Lane.js
@@ -2,13 +2,41 @@ import React from 'react'
 import { fruit } from '../db/data'
 import { getFruitBall } from '../Util/Util'
 
+let left = 0;
+const dist = 500;
+const fruitloop = document.querySelector('.bowling');
+
+function bowl(){
+
+    left +=5;
+    fruitloop.style.transform = `translateX(${left}px)`;
+
+
+
+    if(left !== dist){
+        requestAnimationFrame(bowl);
+    }
+    else if(left == dist){
+        
+    fruitloop.style.transform = `translateX(0px)`;
+    }
+
+}
 
 function Lane() {
+
+
+
+    
+
+
+
   const listed = fruit.map(ball =>
     <li key={ball.id}>
         <img src={getFruitBall(ball)} alt={ball.name} className='bowling'
         />
     </li>
+    
     );
 
     return <ul> { listed } </ul>;
