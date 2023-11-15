3193bc25773ca5c8267f42b543c29b2ed19201e4
Next-Token <SSerpas@ladb.org>
2023-11-15 14:53:45 -0600

    formatting and removal of css sylings

--- fruit-cup/src/Navigation/Nav.js
+++ fruit-cup/src/Navigation/Nav.js
@@ -1,30 +1,19 @@
 import "./Nav.css";
 //import icons from '../icons/*';       <marquee scrollamount="3" behavior="scroll" direction="left" >  </marquee>
-import ICONS from '../index.js';
 import Lane from "../Lane/Lane.js";
 
-
-
 function Nav() {
-
-
 
   return (
     <nav>
 
       <div className="nav-container">
 
-<Lane />
+        <Lane />
        
       </div>
-      
-      
+
       <input type="text" className="search-input" placeholder="Enter fruit name here"  hidden/>
-      
-        
-
-      
-
     </nav>
   );
 }
