da1d3c1ed8dfd53ab7d33725e49881eb4962a723
Next-Token <SSerpas@ladb.org>
2023-11-17 11:56:17 -0600

    setting up con

---
+++ fruit-cup/src/Entry/Entry.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+//TODO: if userid return entry page != return login page
+
+
+
+ function Entry() {
+
+
+
+
+  return (
+
+
+
+
+
+    <div>Entry</div>
+
+
+
+  )
+}
+
+
+
+
+export default Entry
--- fruit-cup/src/Util/Util.js
+++ fruit-cup/src/Util/Util.js
@@ -5,6 +5,61 @@ export function getFruitBall(ball){
     return( ball.name + '.png');
 }
 
+
+
+export function mDBConn(){
+
+
+  const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+  const username = encodeURIComponent("fruitboy");
+
+const password = encodeURIComponent("<password>");
+
+const cluster = "<clusterName>";
+
+const authSource = "<authSource>";
+
+const authMechanism = "<authMechanism>";
+
+let uri =
+
+  `mongodb+srv://${username}:${password}@${cluster}/?authSource=${authSource}&authMechanism=${authMechanism}`;
+
+
+
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+
+
+
+  run().catch(console.dir);
+
+
+
+
+}
+
  function Util() {
 
 
