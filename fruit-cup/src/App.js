7979ef17d1700f4ebbda6aa1a16d2ba7c7bcc7e8
Next-Token <SSerpas@ladb.org>
2023-12-05 15:44:04 -0600

    cleaning up removing  un-req'd work

--- fruit-cup/src/App.js
+++ fruit-cup/src/App.js
@@ -1,32 +1,20 @@
 import Navigation from "./Navigation/Nav";
-import Content from "./Content/Content";
 import Activity from "./Activity/Activity";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Test from "./Testing/Test";
-import  { Selc } from "./Lane/Lane";
-import {Lane} from "./Lane/Lane";
-import { fruitMap } from "./db/data";
+
 
 //<Test/> <Content />
 function App() {
 
-  const selected = fruitMap.get(Selc());
 
-  console.log('here');
-  console.log(selected);
-  console.log('APP.JS');
   return <>
  
   <Header />
   <Navigation />
-
   <Activity />
-
-  
   <Footer />
   </>
-
 }
 
 export default App;
