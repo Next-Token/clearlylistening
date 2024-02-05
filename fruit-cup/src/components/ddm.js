f3a91660c280fc2f97d37e47e2a5dd6e5dc538f8
Next-Token <sethserpas@icloud.com>
2024-02-05 11:46:06 -0600

    prettier sidebar

--- fruit-cup/src/components/ddm.js
+++ fruit-cup/src/components/ddm.js
@@ -10,9 +10,9 @@ const DropdownMenu = () => {
     <div className='traverse'>
     
     
-    <button className='btns' ><p style={{transform: "rotate(90deg)"}}>Home</p></button>
+    <button className='btns' ><p style={{transform: "rotate(90deg)",position: "relative",bottom:"5px"}}>Home</p></button>
     <button className='btns' ><p style={{transform: "rotate(90deg)"}}>Blog</p></button>
-    <button className='btns' ><p style={{transform: "rotate(90deg)" }}>More</p></button>
+    <button className='btns' ><p style={{transform: "rotate(90deg)",position: "relative",bottom:"3px" }}>More</p></button>
     <button className='btns' ><p style={{transform: "rotate(90deg)",position: "relative",bottom:"9px"}}>GitHub</p></button>
 
 
